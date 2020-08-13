f64344f39aa11e67b684b8b85c149f61539d0066
Bobrov <psdzofficial@yandex.ru>
2020-08-13 11:42:46 +0300

    created components/experts folder and new components for experts
    added import for ExpertsIndex,Create,Edit

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,4 +1,3 @@
-
 /**
  * First we will load all of this project's JavaScript dependencies which
  * includes Vue and other libraries. It is a great starting point when
@@ -8,15 +7,25 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
+import VueRouter from 'vue-router';
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
+window.Vue.use(VueRouter);
 
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
+import ExpertsIndex from './components/experts/ExpertsIndex.vue';
+import ExpertsCreate from './components/experts/ExpertsCreate.vue';
+import ExpertsEdit from './components/experts/ExpertsEdit.vue';
 
-const app = new Vue({
-    el: '#app'
-});
+const routes = [
+    {
+        path: '/',
+        components: {
+            expertsIndex: ExpertsIndex
+        }
+    },
+    {path: '/admin/experts/create', component: ExpertsCreate, name: 'createExpert'},
+    {path: '/admin/experts/edit/:id', component: ExpertsEdit, name: 'editExpert'},
+];
+
+const router = new VueRouter({ routes });
+
+const app = new Vue({ router }).$mount('#app');
---
+++ resources/assets/js/components/experts/ExpertsCreate.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "ExpertsCreate"
+    }
+</script>
+
+<style scoped>
+
+</style>
